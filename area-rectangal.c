c47742848174628b445c00eb8a869a37dcc3d397
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-06-01 18:59:24 +0530

    Area of rectangle

---
+++ area-rectangal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,s;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+s=a*b;
+printf("Area = %d",s);
+}
