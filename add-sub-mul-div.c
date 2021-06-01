73ac18a3aa91bf1dae90b8d85ecd4bf3e4889aa4
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-06-01 18:56:49 +0530

    Add files via upload

---
+++ add-sub-mul-div.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,A,S,M,D;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+A=a+b;
+S=a-b;
+M=a*b;
+D=a/b;
+printf("\naddition=%d\nsubtratoin=%d\nmultiply=%d\ndivid=%d",A,S,M,D);
+}
