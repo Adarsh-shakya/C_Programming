49672123edbb92c3e6f4474eaf429a32c430483b
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-06-02 20:59:54 +0530

    Add files via upload

---
+++ EvenOdd.c
@@ -0,0 +1,12 @@
+//to check given nummber is even or odd
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:");
+scanf("%d",&num);
+if(num%2==0)
+ printf("%d  is eVEN",num);
+else
+ printf("%d is ODD",num);
+}
