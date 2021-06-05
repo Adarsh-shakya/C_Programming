0f6725fee4eeef5c5cb6593b2620325240b62e92
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-06-05 20:47:00 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n,m,s; 
+printf("enter frist number:");
+scanf("%d",&n);
+printf("enter second number:");
+scanf("%d",&m);
+s=n+m;
+printf("%d",s);
+return 0;
+}
