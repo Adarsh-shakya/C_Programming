3cf20132fa8daa17f2312c53505e334c2cb9c83f
Adarsh-shakya <75556281+Adarsh-shakya@users.noreply.github.com>
2021-06-05 20:48:39 +0530

    Add files via upload

---
+++ triagle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int b,h,area;
+printf("enter the value of base:");
+scanf("%d",&b);
+printf("enter the value of height:");
+scanf("%d",&h);
+area=1/2*(b*h);
+printf("Area=%d",area);
+}
